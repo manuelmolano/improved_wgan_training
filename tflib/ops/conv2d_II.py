7292338c6c9e1837ab4821470ae285989e344c14
molano <mmolano0@hotmail.com>
2017-10-10 15:29:42 +0200

    add some code for evaluating computation time

--- tflib/ops/conv2d_II.py
+++ tflib/ops/conv2d_II.py
@@ -7,7 +7,7 @@ Created on Sun Oct  1 15:56:13 2017
 """
 
 import tflib as lib
-
+import time
 import numpy as np
 import tensorflow as tf
 
@@ -58,7 +58,7 @@ def Conv2D(name, input_dim, output_dim, filter_size1, filter_size2, inputs, he_i
             input=inputs, 
             filter=filters, 
             strides=[1, 1, stride, stride],
-            padding='SAME',
+            padding='VALID',
             data_format='NCHW'
         )
 
@@ -74,3 +74,51 @@ def Conv2D(name, input_dim, output_dim, filter_size1, filter_size2, inputs, he_i
             return result, filters
         else:
             return result
+        
+        
+        
+if __name__ == '__main__':
+    
+    set_weights_stdev(0.02)
+    start = time.time()
+    num_features = 64
+    kernel_width = 5
+    inputs = np.random.random(size=(64,1, num_features,16)).astype('float32')
+    output = tf.pad(inputs, [[0, 0], [0, 0], [0, 0], [0, kernel_width-1]], "CONSTANT")
+    
+    aux = Conv2D('test', 1, 128, num_features, kernel_width, output, stride=2)
+    print(aux.get_shape())
+    aux = tf.transpose(aux, [0, 2, 1, 3])
+    print(aux.get_shape())
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    for ind in range(10000):
+        test2 = sess.run(aux)
+    print(time.time()-start)
+    print(test2.shape)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
