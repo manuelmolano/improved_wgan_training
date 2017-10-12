e81c71990a04820e6c5c4216189078390283f103
molano <mmolano0@hotmail.com>
2017-10-12 15:54:40 +0200

    funct to produce samples with activity packets

--- tflib/sim_pop_activity.py
+++ tflib/sim_pop_activity.py
@@ -34,13 +34,37 @@ def spike_trains_corr(num_bins=64, num_neurons=32, correlations_mat=np.zeros((16
     assert np.sum(np.isnan(X.flatten()))==0
     return X.astype(float)
 
-def get_samples(num_samples=2**13,num_bins=64, num_neurons=32, correlations_mat=np.zeros((16,))+0.5,\
-                        group_size=2,refr_per=2,firing_rates_mat=np.zeros((16,))+0.2,activity_peaks=np.zeros((16,))+32):                        
+
+def spike_train_packets(num_bins=64, num_neurons=32, group_size=4, num_packets=8, prob_packets=0.02, firing_rates_mat=np.zeros((32,1))+0.05):
+    X = (np.zeros((num_neurons,num_bins)) + firing_rates_mat) > np.random.random((num_neurons,num_bins))
+    packets_activity = np.zeros((num_neurons,num_bins))
+    packet = np.eye(group_size)*2
+    for ind_p in range(int(num_neurons/group_size)):
+        num_packets = np.random.binomial(num_bins,prob_packets)
+        packets_timing = np.sort(np.random.randint(0,num_bins-group_size,size=(num_packets,)))
+        aux = np.zeros((1,num_bins))
+        aux[0,packets_timing] = 1
+        aux = refractory_period_hard(group_size, aux, prob_packets)
+        packets_timing = np.nonzero(aux[0])[0]
+        assert all(np.diff(packets_timing)>group_size)
+        for ind_t in range(len(packets_timing)):
+            packets_activity[ind_p*group_size:(ind_p+1)*group_size,packets_timing[ind_t]:packets_timing[ind_t]+group_size] = packet
+        
+                
+    result = X + packets_activity
+    return result, X, packets_activity
+        
+        
+def get_samples(num_samples=2**13,num_bins=64, num_neurons=32, correlations_mat=np.zeros((16,))+0.5, type_response='corrs',\
+                        group_size=2,refr_per=2,firing_rates_mat=np.zeros((16,))+0.2,activity_peaks=np.zeros((16,))+32, prob_packets=0.05):                        
     X = np.zeros((num_neurons*num_bins,num_samples))
-    
     for ind in range(num_samples):
-        sample = spike_trains_corr(num_neurons=num_neurons,num_bins=num_bins, correlations_mat=correlations_mat,\
+        if type_response=='corrs':
+            sample = spike_trains_corr(num_neurons=num_neurons,num_bins=num_bins, correlations_mat=correlations_mat,\
                     group_size=group_size, firing_rates_mat=firing_rates_mat, refr_per=refr_per, activity_peaks=activity_peaks)
+        elif type_response=='packets':
+            sample = spike_trains_corr(num_bins=num_bins, num_neurons=num_neurons, group_size=group_size, prob_packets=prob_packets, firing_rates_mat=firing_rates_mat)
+            
         X[:,ind] = sample.reshape((num_neurons*num_bins,-1))[:,0]
      
     return X
@@ -63,10 +87,32 @@ def get_aproximate_probs(num_samples=2**13,num_bins=64, num_neurons=32, correlat
     assert abs(np.sum(r_unique[1])-num_samples)<0.00000001
     
     return r_unique
-    
+ 
+def refractory_period_hard(refr_per, r, firing_rate):
+    #print('imposing refractory period of ' + str(refr_per))    
+    margin_length = 2*np.shape(r)[1]
+    for ind_tr in range(int(np.shape(r)[0])):
+        r_aux = r[ind_tr,:]
+        margin1 = np.random.poisson(np.zeros((margin_length,))+firing_rate)
+        margin1[margin1>0] = 1
+        r_aux = np.hstack((margin1,r_aux))
+        spiketimes = np.nonzero(r_aux>0)
+        spiketimes = np.sort(spiketimes)
+        isis = np.diff(spiketimes)
+        too_close = np.nonzero(isis<=refr_per)
+        while len(too_close[0])>0:
+            spiketimes = np.delete(spiketimes,too_close[0][0]+1)
+            isis = np.diff(spiketimes)
+            too_close = np.nonzero(isis<=refr_per)
+        
+        r_aux = np.zeros(r_aux.shape)
+        r_aux[spiketimes] = 1
+            
+        r[ind_tr,:] = r_aux[margin_length:]
+    return r
     
 def refractory_period(refr_per, r, firing_rate):
-    sigma = refr_per/2#np.sqrt(refr_per)
+    sigma = refr_per/1.5#np.sqrt(refr_per)
     #print('imposing refractory period of ' + str(refr_per))    
     margin_length = 2*np.shape(r)[1]
     for ind_tr in range(int(np.shape(r)[0])):
@@ -83,11 +129,9 @@ def refractory_period(refr_per, r, firing_rate):
             spiketimes = np.delete(spiketimes,too_close[0]+1)
             isis = np.diff(spiketimes)
             prob_of_removing = np.exp(-(isis/sigma**2))
-            #print(prob_of_removing[0:20])
-            what_to_remove = np.random.random(size=prob_of_removing.shape)<prob_of_removing
-            #print(what_to_remove[0:20])
+            what_to_remove = np.random.random(size=prob_of_removing.shape)<prob_of_removing           
             too_close = np.nonzero(what_to_remove)
-            #print(too_close[0][0:10])
+         
         r_aux = np.zeros(r_aux.shape)
         r_aux[spiketimes] = 1
             
@@ -97,6 +141,21 @@ def refractory_period(refr_per, r, firing_rate):
 
     
 if __name__ == '__main__':
+    plt.close('all')
+    sample, background, packet_activity = spike_train_packets()
+    sample[sample==3] = 2
+    f = plt.figure()
+    plt.imshow(sample,interpolation='nearest')
+    plt.colorbar()
+    f1 = plt.figure()
+    plt.imshow(background,interpolation='nearest')
+    plt.colorbar()
+    f2 = plt.figure()
+    plt.imshow(packet_activity,interpolation='nearest')
+    plt.colorbar()
+    
+    asdsadds
+    
     import analysis
     num_tr = 1000
     num_bins = 64
@@ -111,4 +170,8 @@ if __name__ == '__main__':
             X[:,ind] = sample.reshape((num_neurons*num_bins,-1))[:,0]
             autocorrelogram_mat += analysis.autocorrelogram(sample,lag=lag)
         autocorrelogram_mat = autocorrelogram_mat/np.max(autocorrelogram_mat)
+        autocorrelogram_mat[lag] = 0 
         plt.plot(autocorrelogram_mat)
+
+
+
